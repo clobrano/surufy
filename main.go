22f34a2246c3e86819f234174dc2bc3f3c0ae862
Carlo Lobrano <c.lobrano@gmail.com>
2018-11-05 00:00:01 +0100

    Draft to colorize the squircle

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+// convert -modulate to change brightness, saturation and hue
+
 import (
 	"gopkg.in/gographics/imagick.v2/imagick"
 )
@@ -7,4 +9,16 @@ import (
 func main() {
 	imagick.Initialize()
 	defer imagick.Terminate()
+
+	mw := imagick.NewMagickWand()
+	mw.ReadImage("squircle2.png")
+
+	colorize := imagick.NewPixelWand()
+	colorize.SetColor("green")
+
+	opacity := imagick.NewPixelWand()
+	opacity.SetColor("rgb(60%,60%,60%)")
+
+	mw.ColorizeImage(colorize, opacity)
+	mw.WriteImage("result.png")
 }
